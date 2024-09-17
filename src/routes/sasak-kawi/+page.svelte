27208e54190b9e446fff4dece5c0641c7ddff258
Your Namear <you@example.com>
2024-09-17 09:54:33 +0800

    perbaikan domain

--- src/routes/sasak-kawi/+page.svelte
+++ src/routes/sasak-kawi/+page.svelte
@@ -7,15 +7,15 @@ import Converter from "$lib/components/Converter.svelte";
 <SvelteSeo
     title="Transliterasi Aksara Sasak | Sasak-Kawi"
     description="Alat transliterasi sederhana untuk mengalihkan/mengubah/mengkonversi teks aksara Sasak menjadi aksara Kawi."
-    canonical="https://arh007.github.io/transliterasi-sasak/sasak-kawi"
+    canonical="https://aksarasasak.id/transliterasi-sasak/sasak-kawi"
     keywords="alat, transliterasi, konversi, alih, aksara, sasak, kawi"
     openGraph={{
         title: "Transliterasi Aksara Sasak | Sasak-Kawi",
         description: "Alat transliterasi sederhana untuk mengalihkan/mengubah/mengkonversi teks aksara Sasak menjadi aksara Kawi.",
-        url: "https://arh007.github.io/transliterasi-sasak/sasak-kawi",
+        url: "https://aksarasasak.id/transliterasi-sasak/sasak-kawi",
         type: "website",
         images: [{
-            url: "https://arh007.github.io/transliterasi-sasak/aksara-sasak.png",
+            url: "https://aksarasasak.id/transliterasi-sasak/aksara-sasak.png",
           },],
         site_name: "Transliterasi Aksara Sasak",
       }}
@@ -23,7 +23,7 @@ import Converter from "$lib/components/Converter.svelte";
         card: "summary_large_image",
         title: "Transliterasi Aksara Sasak | Sasak-Kawi",
         description: "Alat transliterasi sederhana untuk mengalihkan/mengubah/mengkonversi teks aksara Sasak menjadi aksara Kawi.",
-        image: "https://arh007.github.io/transliterasi-sasak/aksara-sasak.png",
+        image: "https://aksarasasak.id/transliterasi-sasak/aksara-sasak.png",
       }}
 />
     
